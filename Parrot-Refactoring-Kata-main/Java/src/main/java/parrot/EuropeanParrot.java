2f7ce0f2a3080d52287b65c009023988ce9f2cb2
Victoria Cruz <121833087+ToriCrux@users.noreply.github.com>
2025-05-22 16:43:33 -0300

    Reestrurando_metodos
    Reestruturando Métodos Complexos que apresentam múltiplas responsabilidades

---
+++ Parrot-Refactoring-Kata-main/Java/src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,13 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot {
+    @Override
+    public double getSpeed() {
+        return getBaseSpeed();
+    }
+
+    @Override
+    public String getCry() {
+        return "Sqoork!";
+    }
+}
